cc78e7509d0423673422e15ebafaac6fbace7eae
ImplementLife <implementlifep@gmail.com>
2024-06-01 23:18:43 +0300

    init project

---
+++ app/src/main/java/il/finassist/android/MainActivity.java
@@ -0,0 +1,13 @@
+package il.finassist.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
